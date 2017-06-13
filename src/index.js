f7ead31bae12aea545f1c6b49978fa68a8a7800a
Sudin Ranjitkar <ranjitkarsudeen14@gmail.com>
2017-06-13 15:59:45 +0545

    first commit on react redux

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sudin Ranjitkar on 6/13/2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Countdown Champ</div>,document.getElementById('root')
+);
