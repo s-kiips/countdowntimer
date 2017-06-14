08968c5d017473a846378a777169126841cb6237
Sudin Ranjitkar <ranjitkarsudeen14@gmail.com>
2017-06-14 11:09:52 +0545

    Clock feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
 import React, {Component} from 'react';
 import './App.css';
+import Clock from './Clock';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            deadline:'December 25, 2017',
+        this.state = {
+            deadline: 'December 25, 2017',
             newDeadline: ''
         }
     }
 
-    changeDeadline(){
-      console.log('state', this.state);
+    changeDeadline() {
+        //console.log('state', this.state);
+        this.setState({deadline: this.state.newDeadline})
     }
 
     render() {
         return (
             <div className="App">
                 <div className="App-title">Countdown to {this.state.deadline}</div>
-                <div>
-                    <div className="Clock-days">14 Days</div>
-                    <div className="Clock-hours">30 Hours</div>
-                    <div className="Clock-minutes">15 Minutes</div>
-                    <div className="Clock-seconds">20 Seconds</div>
-                </div>
+                <Clock
+                    deadline={this.state.deadline}
+                />
                 <div>
                     <input
                         placeholder="new date"
                         // onChange={event=>console.log('event',event.target.value)}
-                        onChange={event=>this.setState({newDeadline:event.target.value})}
+                        onChange={event => this.setState({newDeadline: event.target.value})}
                     />
-                    <button onClick={()=>this.changeDeadline()}>
-                            Submit</button>
+                    <button onClick={() => this.changeDeadline()}>
+                        Submit
+                    </button>
                 </div>
             </div>
         )
