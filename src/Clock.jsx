87790c7f95f2ff92eee4685af887a271cd1cfbb7
Sudin Ranjitkar <ranjitkarsudeen14@gmail.com>
2017-06-16 09:50:54 +0545

    dynamic clock feature

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,24 +2,39 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Clock extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            days:0,
-            hours:0,
-            minutes:0,
-            seconds:0
+        this.state = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
         }
+        //console.log('this.props',this.props);
     }
+
+    getTimeUntil(deadline) {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        console.log('time', time);
+        const seconds = Math.floor((time / 1000) % 60);
+        const minutes = Math.floor((time / 1000 / 60) % 60);
+        const hours = Math.floor(time / (1000 * 60 * 60) % 24);
+        const days = Math.floor(time / ( 1000 * 60 * 60 * 24 ));
+        console.log('seconds',seconds, 'minutes', minutes , 'hours', hours, 'days', days);
+        this.setState({days: days});
+    }
+
+
     render() {
-    return(
-        <div>
-            <div className="Clock-days">{this.state.days} Days</div>
-            <div className="Clock-hours">{this.state.hours} Hours</div>
-            <div className="Clock-minutes">{this.state.minutes} Minutes</div>
-            <div className="Clock-seconds">{this.state.seconds} Seconds</div>
-        </div>
-    )
+        this.getTimeUntil(this.props.deadline);
+        return (
+            <div>
+                <div className="Clock-days">{this.state.days} Days</div>
+                <div className="Clock-hours">{this.state.hours} Hours</div>
+                <div className="Clock-minutes">{this.state.minutes} Minutes</div>
+                <div className="Clock-seconds">{this.state.seconds} Seconds</div>
+            </div>
+        )
     }
 }
 export default Clock;
