b939b57e6630f3f7785df9ce27ef1fd37cf319e1
Sudin Ranjitkar <ranjitkarsudeen14@gmail.com>
2017-06-19 09:56:56 +0545

    count down app completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import './App.css';
 import Clock from './Clock';
 import {Form, FormControl, Button} from 'react-bootstrap';
+
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -25,6 +27,7 @@ class App extends Component {
                 />
                 <Form inline>
                     <FormControl
+                        className="Deadline-input"
                         placeholder="new date"
                         // onChange={event=>console.log('event',event.target.value)}
                         onChange={event => this.setState({newDeadline: event.target.value})}
